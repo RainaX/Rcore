a8518034ff70b7fac6bbfe96303e72682411b7f2
RainaX <xiaofanx@andrew.cmu.edu>
2021-02-13 18:19:04 -0500

    use empty frames to store page state info

--- rCore-Project/os/src/memory/frame/allocator.rs
+++ rCore-Project/os/src/memory/frame/allocator.rs
@@ -1,37 +1,75 @@
 use super::*;
 use crate::memory::*;
-use algorithm::*;
 use lazy_static::*;
 use spin::Mutex;
 
 lazy_static! {
-    pub static ref FRAME_ALLOCATOR: Mutex<FrameAllocator<AllocatorImpl>> = Mutex::new(FrameAllocator::new(Range::from(
+    pub static ref FRAME_ALLOCATOR: Mutex<FrameAllocator> = Mutex::new(FrameAllocator::new(Range::from(
             PhysicalPageNumber::ceil(*KERNEL_END_ADDRESS)..PhysicalPageNumber::floor(MEMORY_END_ADDRESS),
         )
     ));
 }
 
-pub struct FrameAllocator<T: Allocator> {
-    start_ppn: PhysicalPageNumber,
-    allocator: T,
+pub struct FrameAllocator {
+    top_header_addr: PhysicalAddress,
 }
 
-impl<T: Allocator> FrameAllocator<T> {
+#[derive(Copy, Clone)]
+struct FrameHeader {
+    count: usize,
+    next_header_addr: PhysicalAddress,
+}
+
+impl FrameAllocator {
     pub fn new(range: impl Into<Range<PhysicalPageNumber>> + Copy) -> Self {
-        FrameAllocator {
-            start_ppn: range.into().start,
-            allocator: T::new(range.into().len()),
+        let allocator = FrameAllocator {
+            top_header_addr: range.into().start.into(),
+        };
+
+        let top_header = FrameHeader {
+            count: range.into().len(),
+            next_header_addr: PhysicalAddress(0),
+        };
+
+        unsafe {
+            *(allocator.top_header_addr.0 as *mut FrameHeader) = top_header;
         }
+
+        allocator
     }
 
     pub fn alloc(&mut self) -> MemoryResult<FrameTracker> {
-        self.allocator
-            .alloc()
-            .ok_or("no available frame to allocate")
-            .map(|offset| FrameTracker(self.start_ppn + offset))
+        if self.top_header_addr.0 == 0 {
+            return MemoryResult::Err("no available frame to allocate");
+        }
+
+        let mut alloc_page_number: PhysicalPageNumber = self.top_header_addr.into();
+
+        unsafe {
+            let mut top_header = *(self.top_header_addr.0 as *mut FrameHeader);
+
+            if top_header.count > 1 {
+                top_header.count -= 1;
+                alloc_page_number += top_header.count;
+                *(self.top_header_addr.0 as *mut FrameHeader) = top_header;
+            } else {
+                self.top_header_addr = top_header.next_header_addr;
+            }
+        }
+
+        Ok(FrameTracker(alloc_page_number))
     }
 
     pub(super) fn dealloc(&mut self, frame: &FrameTracker) {
-        self.allocator.dealloc(frame.page_number() - self.start_ppn);
+        let new_top_header = FrameHeader {
+            count: 1,
+            next_header_addr: self.top_header_addr,
+        };
+
+        unsafe {
+            *(frame.address().0 as *mut FrameHeader) = new_top_header;
+        }
+
+        self.top_header_addr = frame.address();
     }
 }
